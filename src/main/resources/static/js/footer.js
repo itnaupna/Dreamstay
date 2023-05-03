20f16625beb40b02f5e85c92839e9a32deb233d9
helloa1109 <helloa1109@naver.com>
2023-05-03 17:11:29 +0900

    footer dropdown list

--- src/main/resources/static/js/footer.js
+++ src/main/resources/static/js/footer.js
@@ -42,6 +42,32 @@ $(document).ready(function (){
     });
 });
 
+// dropdwon_list script
+
+$(document).ready(function() {
+    // 드롭다운 리스트 클릭시
+    // active 활성화 하면서  aria-expanded  == true 로 ..
+    $('.dropdown_list').click(function(event) {
+        // 클릭 이벤트 버블링 막음
+        event.stopPropagation();
+        // 다른 dropdown  list : active , aria-expanded 요소 false 처리
+        $('.dropdown_list').not(this).removeClass('active').attr('aria-expanded', 'false');
+        $(this).toggleClass('active');
+        var expanded = $(this).attr('aria-expanded') === 'true' || false;
+        $(this).attr('aria-expanded', !expanded);
+        $('.item_list').not($(this).children('.item_list')).slideUp(300);
+        $(this).children('.item_list').slideToggle(300);
+    });
+
+    // 다른 곳을 클릭하면 드롭다운 닫는 코드
+    $(document).click(function() {
+        $('.dropdown_list').removeClass('active').attr('aria-expanded', 'false');
+        $('.item_list').slideUp(300);
+    });
+});
+
+
+
 
 
 
