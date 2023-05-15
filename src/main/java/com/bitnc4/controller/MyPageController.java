b200d050f99b88a013c54b498ec2ff0827eea7d1
helloa1109 <helloa1109@naver.com>
2023-05-15 14:15:47 +0900

    마이페이지 세션 수정

--- src/main/java/com/bitnc4/controller/MyPageController.java
+++ src/main/java/com/bitnc4/controller/MyPageController.java
@@ -28,22 +28,25 @@ public class MyPageController {
     @GetMapping("")
     public String mypage(HttpSession session, Model model)
     {
-        String id = String.valueOf(session.getAttribute("userid"));
+        String id = ((MemberDto)session.getAttribute("loginuser")).getId();
         model.addAttribute("memberDto",mypageService.selectInfoToId(id));
         List<HotelDto> list = adminHnRService.getHotels();
         model.addAttribute("list",list);
+
         return "/mypage/mypage";
     }
 
     @GetMapping("/updateinfo")
     public String updateinfo(HttpSession session,Model model)
     {
-        MemberDto dto = mypageService.selectInfoToId(String.valueOf(session.getAttribute("userid")));
+        MemberDto dto = (MemberDto)(session.getAttribute("loginuser"));
+
         String[] fnFn = dto.getUser_name().split("/");
-        dto.setUser_name(dto.getUser_name().replaceAll("/", ""));
+
         model.addAttribute("memberDto", dto);
         model.addAttribute("familyname", fnFn[0]);
         model.addAttribute("firstname", fnFn[1]);
+
         return "/mypage/updateinfo";
     }
 
@@ -51,14 +54,14 @@ public class MyPageController {
     @ResponseBody
     public void changeinfo(HttpSession session,Model model,MemberDto dto)
     {
-        dto.setId(String.valueOf(session.getAttribute("userid")));
+        dto.setId(((MemberDto)session.getAttribute("loginuser")).getId());
         mypageService.updateUserInfo(dto);
     }
 
     @GetMapping("/updatepass")
     public String updatepass(HttpSession session,Model model)
     {
-        String id = String.valueOf(session.getAttribute("userid"));
+        String id = ((MemberDto)session.getAttribute("loginuser")).getId();
         model.addAttribute("memberDto",mypageService.selectInfoToId(id));
         return "/mypage/updatepass";
     }
@@ -67,14 +70,14 @@ public class MyPageController {
     @ResponseBody
     public int changepw(HttpSession session, String chpw)
     {
-        String id = String.valueOf(session.getAttribute("userid"));
+        String id = ((MemberDto)session.getAttribute("loginuser")).getId();
         return mypageService.updateUserPass(id,chpw);
     }
 
     @GetMapping("/deleteform")
     public String deleteform(HttpSession session, Model model) {
-        String id = String.valueOf(session.getAttribute("userid"));
-        model.addAttribute("memberDto",mypageService.selectInfoToId(id));
+        MemberDto dto = (MemberDto)session.getAttribute("loginuser");
+        model.addAttribute("memberDto", dto);
     return "/mypage/deleteform";
     }
 
@@ -82,31 +85,23 @@ public class MyPageController {
     @PostMapping("/chkidpw")
     @ResponseBody
     public int chkidpw(HttpSession session, String pw) {
-        String id = String.valueOf(session.getAttribute("userid"));
+        String id = ((MemberDto)session.getAttribute("loginuser")).getId();
         return mypageService.matchForIdPw(id, pw);
     }
 
     @GetMapping("/deleteuser")
     public String deleteuser(HttpSession session)
     {
-        mypageService.deleteUser(String.valueOf(session.getAttribute("userid")));
-        session.removeAttribute("userid");
+        mypageService.deleteUser(((MemberDto)session.getAttribute("loginuser")).getId());
+        session.removeAttribute("loginuser");
         return "redirect:/";
     }
 
     @GetMapping("/membership")
     public String myreservation(HttpSession session, Model model)
     {
-        String id = String.valueOf(session.getAttribute("userid"));
+        String id = ((MemberDto)session.getAttribute("loginuser")).getId();
         model.addAttribute("memberDto",mypageService.selectInfoToId(id));
         return "/mypage/membership";
     }
-
-    @GetMapping("/kakao")
-    public String mypagelg()
-    {
-        return "/mypage/kakao";
-    }
-
-
 }
